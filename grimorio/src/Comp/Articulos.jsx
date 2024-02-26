7d1522ee5b96a7395d0aa4b3b3d13c96f8472dac
Linx306 <Hassam306@gmail.com>
2024-02-25 21:48:26 -0700

    Se elimino un css
    Se elimino un css de articulos por ser redundante

--- grimorio/src/Comp/Articulos.jsx
+++ grimorio/src/Comp/Articulos.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import './Articulos.css';
 
 function Articulos({ nombre, descripcion }) {
   return (
-    <div className="artilugio-container">
+    <div>
       <h3>{nombre}</h3>
       <p>{descripcion}</p>
     </div>
