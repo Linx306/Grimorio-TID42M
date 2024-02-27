0ccab2c5ed676ab8cf72e65c90c585157446bfb7
Linx306 <Hassam306@gmail.com>
2024-02-27 16:51:52 -0700

    Dev
    Se cambiron cosas en el dev

--- grimorio/src/Comp/Dev.jsx
+++ grimorio/src/Comp/Dev.jsx
@@ -1,4 +1,7 @@
 const Dev = () => {
-    return <h1>Dev</h1>;
+    return<div>
+        <h1>Dev</h1>;
+        <p>Pagina de dev que se le implementaran funciones en el futuro</p>
+    </div> 
 }
 export default Dev
